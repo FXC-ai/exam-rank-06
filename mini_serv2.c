16e05fa43f05873e9d9ea314af66f188a5c6bfc7
Francois-xavier Coindreau <fcoindre@student.42.fr>
2024-02-13 20:08:23 +0100

    safety push

--- mini_serv2.c
+++ mini_serv2.c
@@ -5,12 +5,12 @@
 
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <sys/select.h>
+
 
 #define MAGENTA "\033[1;95m"
 #define RESET "\033[0m"
 
-#define PORT 8081
+#define PORT 8083
 
 void displaySockaddr_in(struct sockaddr_in* addr)
 {
@@ -22,46 +22,25 @@ void displaySockaddr_in(struct sockaddr_in* addr)
 
 }
 
-void run_server ()
+
+int main()
 {
 	int socketServer;
 	int socketClient;
 
 	struct sockaddr_in addrServer;
+
 	int addrServer_len = sizeof(addrServer);
 
 	socketServer = socket(AF_INET, SOCK_STREAM, 0);
 	printf("Socket server created : %d\n", socketServer);	
-	
+
 	addrServer.sin_family = AF_INET;
 	addrServer.sin_port = htons(PORT);
 	addrServer.sin_addr.s_addr = INADDR_ANY;
 
 	printf("Socket server addrServer : \n");
 	displaySockaddr_in(&addrServer);
-
-	int bind_result = bind(socketServer, (struct sockaddr*)&addrServer, sizeof(addrServer));
-	printf("Bind result : %d\n", bind_result);
-
-	int listen_result = listen(socketServer, 10);
-	printf("Listen result : %d\n", listen_result);
-
-
-	socketClient = accept(socketServer, (struct sockaddr*)&addrServer, (socklen_t *)&addrServer_len);
-	printf("Socket client created : %d\n", socketClient);
-
-	char buf[1024];
-	int recv_status = recv(socketClient, buf, 1024, 0);
-
-	printf("Received (%d): %s\n",recv_status, buf);
-
-	close(socketServer);
-	close(socketClient);
-}
-
-
-int main()
-{
 
 	int socketOutside = socket(AF_INET, SOCK_STREAM, 0);
 	printf("Socket Outside created : %d\n", socketOutside);
@@ -74,8 +53,12 @@ int main()
 	printf("Socket Outside addrServer : \n");
 	displaySockaddr_in(&addrOutside);
 
-	printf("--------------Server start-------------------\n");
-	run_server();
+
+	int bind_result = bind(socketServer, (struct sockaddr*)&addrServer, sizeof(addrServer));
+	printf("Bind result : %d\n", bind_result);
+
+	int listen_result = listen(socketServer, 10);
+	printf("Listen result : %d\n", listen_result);
 
 	int connect_status = connect(socketOutside, (struct sockaddr*)&addrOutside, sizeof(addrOutside));
 	printf("Connect status : %d\n", connect_status);
@@ -83,8 +66,16 @@ int main()
 	int send_status = send(socketOutside, "Hello", 6, 0);
 	printf("Send status : %d\n", send_status);
 
+	socketClient = accept(socketServer, (struct sockaddr*)&addrServer, (socklen_t *)&addrServer_len);
+	printf("Socket client created : %d\n", socketClient);
 
+	char buf[1024];
+	int recv_status = recv(socketClient, buf, 1024, 0);
 
+	printf("Received (%d): %s\n",recv_status, buf);
+
+	close(socketServer);
+	close(socketClient);
 	close(socketOutside);
 	return 0;
 }
