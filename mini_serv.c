4bb523a0cd00ffb8f5e21ee745162726f549460f
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2024-02-13 18:38:20 +0100

    EOD

--- mini_serv.c
+++ mini_serv.c
@@ -8,27 +8,9 @@
 
 
 #define MAGENTA "\033[1;95m"
-#define RED "\033[1;91m"
-#define GREEN "\033[1;92m"
-#define BLUE "\033[1;94m"
-#define YELLOW "\033[1;93m"
-#define CYAN "\033[1;96m"
 #define RESET "\033[0m"
-// union mini_serv_data
-// {
-// 	int i;
-// 	char c;
-// };
 
-// struct mini_serv_data_struct
-// {
-// 	int i;
-// 	char c;
-// 	union mini_serv_data data;
-
-// };
-
-#define PORT 8080
+#define PORT 8083
 
 void displaySockaddr_in(struct sockaddr_in* addr)
 {
@@ -43,10 +25,8 @@ void displaySockaddr_in(struct sockaddr_in* addr)
 
 int main()
 {
-
 	int socketServer;
 	int socketClient;
-	// char buf[1024];
 
 	struct sockaddr_in addrServer;
 
@@ -56,18 +36,12 @@ int main()
 	printf("Socket server created : %d\n", socketServer);	
 
 	addrServer.sin_family = AF_INET;
-	// printf("AF_INET = %d\n", AF_INET);	
-
 	addrServer.sin_port = htons(PORT);
-	// printf("PORT = %d\n", addrServer.sin_port);
-
 	addrServer.sin_addr.s_addr = INADDR_ANY;
-	// printf("INADDR_ANY = %d\n", INADDR_ANY);
+
 
 	printf("Socket server addrServer : \n");
 	displaySockaddr_in(&addrServer);
-
-
 
 	int bind_result = bind(socketServer, (struct sockaddr*)&addrServer, sizeof(addrServer));
 	printf("Bind result : %d\n", bind_result);
@@ -81,7 +55,7 @@ int main()
 	struct sockaddr_in addrOutside;
 	addrOutside.sin_family = AF_INET;
 	addrOutside.sin_port = htons(PORT);
-	addrOutside.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addrOutside.sin_addr.s_addr = inet_addr("10.11.6.3");
 
 	printf("Socket Outside addrServer : \n");
 	displaySockaddr_in(&addrOutside);
@@ -93,55 +67,15 @@ int main()
 	printf("Send status : %d\n", send_status);
 
 	socketClient = accept(socketServer, (struct sockaddr*)&addrServer, (socklen_t *)&addrServer_len);
-
+	printf("Socket client created : %d\n", socketClient);
 
 	char buf[1024];
 	int recv_status = recv(socketClient, buf, 1024, 0);
 
 	printf("Received (%d): %s\n",recv_status, buf);
 
-
-
-	// union mini_serv_data data;
-
-	// data.i = 10;
-
-	// struct mini_serv_data_struct data_struct;
-
-	// data_struct.i = 10;
-
-	// data_struct.data.i = 10;
-
-	// struct sockaddrServer_in addr;
-	
-	// int sock1;
-	// int fd = open("test.txt", O_RDWR);
-
-	// write(fd, "Hello", 6);
-	// read(fd, buf, 1024);
-
-	// printf("Read : %s\n", buf);
-
-	// close(fd);
-
-	// sock1 = socket(AF_INET, SOCK_STREAM, 0);
-	
-
-	// printf("Socket created : %d\n", sock0);
-	// printf("Socket created : %d\n", sock1);
-
-	// ssize_t nb_chr_write = write(sock0, "Hello", 6);
-
-	// printf("Write %zd bytes\n", nb_chr_write);
-
-	// char buf[1024];
-
-	// int nb_char_read = read(sock0, buf, 1024);
-
-	// printf("Read %d bytes : %s\n", nb_char_read, buf);
-
-	// close(sock0);
-	// close(sock1);
-
+	close(socketServer);
+	close(socketClient);
+	close(socketOutside);
 	return 0;
 }
